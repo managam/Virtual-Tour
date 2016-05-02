74e452534a887da092e87b698b40103cc81256c5
Managam Silalahi <managam.silalahi@gmail.com>
2016-05-02 14:15:17 +0700

    Tab Map finish.

---
+++ Virtual Tour/Virtual Tour/RealEstate.swift
@@ -0,0 +1,35 @@
+//
+//  RealEstate.swift
+//  Virtual Tour
+//
+//  Created by Admin on 5/2/16.
+//  Copyright © 2016 Morra. All rights reserved.
+//
+
+import Foundation
+
+class RealEstate {
+    
+    // properties and methods
+    // attributes / properties
+    var developer: String = "Eton Property"
+    var name: String = "Reflections"
+    var price: String = "$395,000"
+    var type: String = "Apartement"
+    var location: String = "108 Haines Street, North Melbourne, Vic 3051"
+    var image: [String] = ["frontview.jpg", "lounge.jpg", "livingroom.jpg", "garden.jpg", "kitchen.jpg", "bedroom.jpg", "bathroom.jpg", "sideview.jpg", "conservatory.jpg"]
+    
+    // constructor default
+    /* init(developer: String, name: String, description: String, price: String, type: String, location: String, image: [String]) {
+        
+        self.developer = developer
+        self.name = name
+        self.description = description
+        self.price = price
+        self.type = type
+        self.location = location
+        self.image = image
+        
+    } */
+    
+}
---
+++ Virtual Tour/Virtual Tour/VRTourViewController.swift
@@ -0,0 +1,13 @@
+//
+//  VRTourViewController.swift
+//  Virtual Tour
+//
+//  Created by Admin on 5/2/16.
+//  Copyright © 2016 Morra. All rights reserved.
+//
+
+import UIKit
+
+class VRTourViewController: UIViewController {
+
+}
