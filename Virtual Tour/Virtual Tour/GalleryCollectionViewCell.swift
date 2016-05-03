81d5cf29f4b79d33450f953190ae9542adc7c89a
Managam Silalahi <managam.silalahi@gmail.com>
2016-05-03 10:36:40 +0700

    Design Gallery, add GalleryViewController and GalleryCollectionViewController.

---
+++ Virtual Tour/Virtual Tour/GalleryCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  GalleryCollectionViewCell.swift
+//  Virtual Tour
+//
+//  Created by Admin on 5/3/16.
+//  Copyright © 2016 Morra. All rights reserved.
+//
+
+import UIKit
+
+class GalleryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageGallery: UIImageView!
+
+}
---
+++ Virtual Tour/Virtual Tour/GalleryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GalleryViewController.swift
+//  Virtual Tour
+//
+//  Created by Admin on 5/3/16.
+//  Copyright © 2016 Morra. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+
+    @IBOutlet weak var galleryCollectionView: UICollectionView!
+    
+}
