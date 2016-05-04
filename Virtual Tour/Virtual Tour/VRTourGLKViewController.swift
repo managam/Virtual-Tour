5056e8b5bc46dd67b6aa367cba1fdba5c1c514ed
Managam Silalahi <managam.silalahi@gmail.com>
2016-05-04 16:56:17 +0700

    Next, Previous, and title with room name finish.

--- Virtual Tour/Virtual Tour/VRTourGLKViewController.swift
+++ Virtual Tour/Virtual Tour/VRTourGLKViewController.swift
@@ -12,6 +12,17 @@ import GLKit
 class VRTourGLKViewController: GLKViewController {
     
     var panoramaView = PanoramaView()
+    var index = 0
+    
+    let panoramaImages = [
+        "frontview.jpg",
+        "lounge.jpg",
+        "livingroom.jpg",
+        "kitchen.jpg",
+        "bedroom.jpg",
+        "location.jpg",
+        ]
+    
     
     override func prefersStatusBarHidden() -> Bool {
         return true
@@ -19,17 +30,7 @@ class VRTourGLKViewController: GLKViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let panoramaView = self.panoramaView as PanoramaView! {
-            
-            panoramaView.setImage(UIImage(named: "frontview.jpg")) // (4096×2048), 2048×1024, 1024×512, 512×256, 256×128
-            panoramaView.touchToPan = false          // Use touch input to pan
-            panoramaView.orientToDevice = true     // Use motion sensors to pan
-            panoramaView.pinchToZoom = false         // Use pinch gesture to zoom
-            panoramaView.showTouches = false         // Show touches
-            self.view = panoramaView
-            
-        }
+        setPanorama(index)
     }
     
     override func glkView(view: GLKView, drawInRect rect: CGRect) {
@@ -37,6 +38,49 @@ class VRTourGLKViewController: GLKViewController {
         self.panoramaView.draw()
         
     }
+    
+    func setPanorama(index: Int) {
+        
+        let panorama = panoramaImages[index]
+        
+        if let panoramaView = self.panoramaView as PanoramaView! {
+            
+            panoramaView.setImage(UIImage(named: panorama)) // (4096×2048), 2048×1024, 1024×512, 512×256, 256×128
+            panoramaView.touchToPan = false          // Use touch input to pan
+            panoramaView.orientToDevice = true     // Use motion sensors to pan
+            panoramaView.pinchToZoom = false         // Use pinch gesture to zoom
+            panoramaView.showTouches = false         // Show touches
+            
+            self.view = panoramaView
+            title = panorama.substringWithRange(Range<String.Index>(start: panorama.startIndex.advancedBy(0), end: panorama.endIndex.advancedBy(-4))).capitalizedString
+            
+        }
+    }
+    
+    
+    @IBAction func navigationPanorama(sender: UIBarButtonItem) {
+        
+        switch sender.title! {
+        case "Next":
+            if index >= 0 && index < panoramaImages.count - 1 {
+                index += 1
+                setPanorama(index)
+            }
+        case "Previous":
+            if index > 0 && index <= panoramaImages.count - 1 {
+                index -= 1
+                setPanorama(index)
+            }
+            
+        default:
+            break
+        }
+        
+        
+        
+    }
+    
+    
     
     
     
